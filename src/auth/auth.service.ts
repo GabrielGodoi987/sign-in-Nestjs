e77815634a673134f59c8ee44e1790c8b7102f7a
gabrielgodoi987 <gabrielgodoi987@gmail.com>
2024-12-06 11:35:08 -0300

    .env.example + updates at auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/users/models/users.model';
+import { sign } from 'jsonwebtoken';
 
 @Injectable()
 export class AuthService {
-    constructor(private jwtService: JwtService){}
+  constructor(
+    private jwtService: JwtService,
+    @InjectModel('User') private readonly userModel: Model<User>,
+  ) {}
+
+  public async createAcessToken(userId: string): Promise<string> {
+    return sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRATION,
+    });
+  }
+
+  public async loginUser() {}
 }
